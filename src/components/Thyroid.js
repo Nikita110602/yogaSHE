6b3c47786404d62c70774f8a613d859477b3a6e5
Nikita110602 <nikitasharma110602@gmail.com>
2023-05-09 19:39:19 +0530

    third day

--- src/components/Thyroid.js
+++ src/components/Thyroid.js
@@ -10,14 +10,34 @@ const Thyroid = () => {
         <img src="https://images-prod.healthline.com/hlcmsresource/images/400x400_10_Yoga_Poses_That_Can_Improve_the_Health_of_your_Thyroid_Supported_Shoulderstand.gif" alt="" />
         </div>
         <div className='posterName'>
-        <h1>1. Sarvangasana or Shoulder Stand</h1>
-        <p>Sarvangasana is referred to as the “Queen of Asanas’. The inverted pose revitalizes blood flow to the glands present in the upper body and regulates metabolism. Moreover, the way the chin is tucked into your chest benefits thyroid functions in the human body. Thus, this is one of the best yoga asanas for improving thyroid health.</p>
+        <h1>1. Sarvangasana </h1>
+        <p>Sarvangasana is referred to as the “Queen of Asanas’. 
+        The inverted pose revitalizes blood flow to the glands 
+        present in the upper body and regulates metabolism. Moreover,
+         the way the chin is tucked into your chest benefits thyroid 
+         functions in the human body. Thus, this is one of the best yoga asanas for improving thyroid health.</p>
         </div>
     </div>
-    {/* <div className='steps'>
-    <p>Sarvangasana is referred to as the “Queen of Asanas’. The inverted pose revitalizes blood flow to the glands present in the upper body and regulates metabolism. Moreover, the way the chin is tucked into your chest benefits thyroid functions in the human body. Thus, this is one of the best yoga asanas for improving thyroid health.</p>
-
-    </div> */}
+    <div className="asan">
+    <div className="img1">
+        <img src="https://images-prod.healthline.com/hlcmsresource/images/400x400_10_Yoga_Poses_That_Can_Improve_the_Health_of_your_Thyroid_Plow_Pose.gif" alt="" />
+        </div>
+        <div className='Name'>
+        <h1>2.Halasana </h1>
+        <p>Plow Pose, referred to as Halasana in Sanskrit, is an inverted yoga asana that strengthens, stretches and relaxes your body. This asana stretches the neck and thus stimulates the thyroid and pituitary glands. However, individuals suffering from hyperthyroidism should not perform this yoga asana as thyroid hormones are secreted during this pose.</p>
+        </div>
+    </div>
+    <div className="asan">
+    <div className="img1">
+        <img src="https://images-prod.healthline.com/hlcmsresource/images/400x400_10_Yoga_Poses_That_Can_Improve_the_Health_of_your_Thyroid_Fish_Pose.gif" alt="" />
+        </div>
+        <div className='Name'>
+        <h1>3. Matsyasana</h1>
+        <p>When it comes to yoga poses for improving thyroid health, Matsyasana is one of the best asanas for reversing hypothyroidism. It involves stretching the throat and neck, which stimulates the thyroid glands. This pose increases the blood flow in the thyroid glands and is beneficial for individuals suffering from hypothyroidism.</p>
+        </div>
+        
+    </div>
+    
   </>
 }
 
